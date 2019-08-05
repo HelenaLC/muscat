cea6743a7e781ebfb08805f6427d551b98629b82
Helena Lucia Crowell <hlc@Helenas-iMac.local>
2019-08-05 13:09:47 +0200

    add wrappers for validity checks of function arguments

--- R/validity-checks.R
+++ R/validity-checks.R
@@ -26,44 +26,61 @@
 #   y = SCE containing pseudo-bulks as returned by 
 #`      aggregateData(x, by = c("cluster_id", "sample_id"))
 #' @importFrom methods is
-#' @importFrom SummarizedExperiment assayNames 
-.check_pb <- function(x, y) {
-    stopifnot(is(y, "SingleCellExperiment"))
-    stopifnot(all.equal(assayNames(y), levels(x$cluster_id)))
-    stopifnot(all.equal(colnames(y), levels(x$sample_id)))
-    stopifnot(all.equal(rownames(y), rownames(x)))
+#' @importFrom S4Vectors metadata
+#' @importFrom SummarizedExperiment assayNames
+.check_pbs <- function(pbs, sce = NULL, check_by = TRUE) {
+    stopifnot(is(pbs, "SingleCellExperiment"))
+    stopifnot(!is.null(ei <- metadata(pbs)$experiment_info))
+    stopifnot(!is.null(agg_pars <- metadata(pbs)$agg_pars))
+    stopifnot(!is.null(n_cells <- metadata(pbs)$n_cells))
+    stopifnot(identical(assayNames(pbs), rownames(n_cells)))
+    stopifnot(identical(colnames(pbs), colnames(n_cells)))
+    if (!is.null(sce)) {
+        stopifnot(identical(ei, metadata(sce)$experiment_info))
+        stopifnot(identical(rownames(pbs), rownames(sce)))
+        stopifnot(identical(assayNames(pbs), levels(sce[[agg_pars$by[1]]])))
+        if (length(agg_pars$by == 2))
+            stopifnot(identical(colnames(pbs), levels(sce[[agg_pars$by[2]]])))
+    }
+    if (check_by)
+        stopifnot(!is.null(pbs[["group_id"]]),
+            identical(agg_pars$by, c("cluster_id", "sample_id")))
 }
 
 # check validity of runDS() output
+#' @importFrom methods is
+#' @importFrom S4Vectors metadata
 .check_res <- function(x, y) {
     ei <- metadata(x)$experiment_info
     nk <- length(kids <- levels(x$cluster_id))
     nms <- c("table", "data", "method", "design", "contrast", "coef")
     stopifnot(is(y, "list"), all.equal(names(y), nms))
     # table
-    stopifnot(is(y$table, "list"))
-    stopifnot(vapply(y$table, function(u) is(u, "list"), logical(1)))
-    stopifnot(identical(names(y$table), colnames(y$contrast))
-        | identical(names(y$table), names(y$coef)))
-    stopifnot(apply(vapply(y$table, names, character(nk)), 2, identical, kids))
+    stopifnot(is(y$table, "list"),
+        vapply(y$table, is, class = "list", logical(1)),
+        identical(names(y$table), colnames(y$contrast))
+        | identical(names(y$table), names(y$coef)),
+        apply(vapply(y$table, names, character(nk)), 2, identical, kids))
     # data
-    stopifnot(is(y$data, "list"))
-    stopifnot(names(y$data) %in% kids)
-    stopifnot(vapply(y$data, function(u) is(u, "DGEList"), logical(1)))
+    stopifnot(is(y$data, "list"), names(y$data) %in% kids,
+        vapply(y$data, is, class = "DGEList", logical(1)))
     # design
-    stopifnot(is(y$design, "matrix"))
-    stopifnot(colnames(y$design) %in% ei$group_id)
-    stopifnot(rownames(y$design) %in% ei$sample_id)
+    stopifnot(is(y$design, "matrix"),
+        colnames(y$design) %in% ei$group_id,
+        rownames(y$design) %in% ei$sample_id)
     # contrast & coef
     stopifnot(is.null(y$contrast) | is(y$contrast, "matrix"))
     stopifnot(is.null(y$coef) | is(y$coef, "numeric") | is(y$coef, "list"))
 }
 
 # check validity of calcExprFreqs() output
+#' @importFrom methods is
+#' @importFrom SummarizedExperiment assays colData
 .check_frq <- function(x, y) {
-    stopifnot(is(x, "SingleCellExperiment"))
-    stopifnot(is(y, "SummarizedExperiment"))
-    stopifnot(identical(levels(x$cluster_id), assayNames(y)))
+    stopifnot(
+        is(x, "SingleCellExperiment"), 
+        is(y, "SingleCellExperiment"))
+    kids <- levels(x$cluster_id)
     
     ids <- levels(x$sample_id)
     if ("group_id" %in% colnames(colData(x)))
@@ -72,4 +89,65 @@
     
     vals <- unlist(assays(y))
     stopifnot(all(vals <= 1), all(vals >= 0))
+}
+
+.check_args_simData <- function(u) {
+    stopifnot(
+        is.numeric(u$n_genes), length(u$n_genes) == 1,
+        is.numeric(u$n_cells), length(u$n_cells) == 1 | length(u$n_cells) == 2,
+        is.numeric(u$p_dd), length(u$p_dd) == 6, sum(u$p_dd) == 1,
+        is.numeric(u$p_type), length(u$p_type) == 1, u$p_type >= 0,
+        is.numeric(u$lfc), is.numeric(u$lfc), u$lfc > 1)
+    if (!is.null(u$rel_lfc)) {
+        nk <- length(kids <- levels(u$cluster_id))
+        stopifnot(is.numeric(u$rel_lfc), 
+            length(u$rel_lfc) == nk, u$rel_lfc >= 0)
+        if (!is.null(nms <- names(u$rel_lfc)))
+            stopifnot(setequal(nms, kids))
+    }
+}
+
+#' @importFrom SummarizedExperiment colData
+.check_args_aggData <- function(u) {
+    stopifnot(is.character(u$by), length(u$by) <= 2, 
+        u$by %in% colnames(colData(u$x)))
+    stopifnot(is.logical(u$scale), length(u$scale) == 1)
+    if (u$scale & (!u$assay %in% c("cpm", "CPM") | u$fun != "sum"))
+        stop("Option 'scale = TRUE' only valid for", 
+            " 'assay = \"cpm/CPM\"' and 'fun = \"sum\"'.")
+}
+
+.check_args_pbDS <- function(u) {
+    if (!is.null(u$design))
+        stopifnot(is.matrix(u$design),
+            !is.null(rownames(u$design)),
+            !is.null(colnames(u$design)))
+    stopifnot(
+        is.null(u$contrast) | is.matrix(u$contrast),
+        is.null(u$coef) | is.numeric(u$coef),
+        is.numeric(u$min_cells), length(u$min_cells) == 1,
+        is.logical(u$verbose), length(u$verbose) == 1)
+}
+
+.check_args_pbHeatmap <- function(u) {
+    if (!is.null(u$k))
+        stopifnot(is.character(u$k), u$k %in% levels(u$x$cluster_id))
+    if (!is.null(u$g))
+        stopifnot(is.character(u$g), u$g %in% rownames(u$x))
+    if (!is.null(u$c))
+        stopifnot(is.character(u$c), u$c %in% names(u$y$table))
+    stopifnot(
+        is.numeric(u$top_n), length(u$top_n) == 1, u$top_n > 1,
+        is.numeric(u$fdr), length(u$fdr) == 1, u$fdr > 0,
+        is.numeric(u$lfc), length(u$lfc) == 1)
+    stopifnot(
+        is.character(u$sort_by), 
+        length(u$sort_by) == 1,
+        u$sort_by %in% names(u$y$table[[1]][[1]]),
+        is.numeric(u$y$table[[1]][[1]][[u$sort_by]]))
+    stopifnot(is.function(u$fun))
+    stopifnot(is.logical(u$normalize), length(u$normalize) == 1)
+    stopifnot(
+        is.logical(u$row_anno), length(u$row_anno) == 1,
+        is.logical(u$col_anno), length(u$col_anno) == 1)
 }
