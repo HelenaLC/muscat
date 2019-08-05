54ffa90ca2fa5cb8b93650b1f8de043ef25a1a3b
Helena Lucia Crowell <hlc@Helenas-iMac.local>
2019-08-05 13:11:45 +0200

    prefix helpers with .

--- R/utils-pbDS.R
+++ R/utils-pbDS.R
@@ -23,7 +23,7 @@
 #   tt: topTable data.frame
 #   ct: comparison type; "contrast" or "coef"
 #   c:  character string specifying the comparison
-res_df <- function(k, tt, ct, c) {
+.res_df <- function(k, tt, ct, c) {
     df <- data.frame(
         gene = rownames(tt), cluster_id = k, tt,
         row.names = NULL, stringsAsFactors = FALSE)
