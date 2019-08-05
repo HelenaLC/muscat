85e1c765bf18624ad204bf5c5ea79149ef939c97
Helena Lucia Crowell <hlc@Helenas-iMac.local>
2019-08-05 13:10:53 +0200

    update dox; add checks; prettify code

--- R/pbMDS.R
+++ R/pbMDS.R
@@ -1,10 +1,14 @@
 #' @rdname pbMDS
-#' @title Pseudo-bulk level MDS plot
+#' @title Pseudobulk-level MDS plot
 #' 
-#' @description ...
+#' @description Renders a multidimensional scaling (MDS) 
+#'   where each point represents a cluster-sample instance; 
+#'   with points colored by cluster ID and shaped by group ID. 
 #' 
 #' @param x a \code{\link[SingleCellExperiment]{SingleCellExperiment}} 
-#'   containing pseudobulk data as returned by \code{\link{aggregateData}}.
+#'   containing cluster-sample pseudobulks as returned by 
+#'   \code{\link{aggregateData}} with argument 
+#'   \code{by = c("cluster_id", "sample_id")}.
 #' 
 #' @return a \code{ggplot} object.
 #' 
@@ -17,16 +21,18 @@
 #' 
 #' @import ggplot2
 #' @importFrom edgeR calcNormFactors cpm DGEList plotMDS.DGEList
-#' @importFrom dplyr bind_cols
 #' @importFrom grDevices colorRampPalette
 #' @importFrom Matrix rowSums
-#' @importFrom methods as
 #' @importFrom SummarizedExperiment assays
 #' @importFrom S4Vectors metadata
 #' @export
 
 pbMDS <- function(x) {
-    y <- as(assays(x), "list")
+    # check validity of input pseudobulk-SCE
+    # (cells should have been aggregated by cluster-sample)
+    .check_pbs(x, check_by = TRUE)
+    
+    y <- as.list(assays(x))
     y <- do.call("cbind", y)
     rmv <- rowSums(y) == 0
     y <- y[!rmv, ]
@@ -34,13 +40,11 @@ pbMDS <- function(x) {
     d <- calcNormFactors(d)
     
     mds <- plotMDS.DGEList(d, plot = FALSE)
-    ei <- metadata(x)$experiment_info
-    m <- match(colnames(x), ei$sample_id)
     nk <- length(kids <- assayNames(x))
     
     df <- data.frame(MDS1 = mds$x, MDS2 = mds$y, 
         cluster_id = factor(rep(kids, each = ncol(x)), levels = kids),
-        group_id = rep(ei$group_id[m], nk))
+        group_id = rep(x$group_id, nk))
     
     cols <- .cluster_colors
     if (nk > length(cols)) 
@@ -49,7 +53,7 @@ pbMDS <- function(x) {
     ggplot(df, aes_string(x = "MDS1", y = "MDS2", 
         col = "cluster_id", shape = "group_id")) +
         scale_color_manual(values = cols) +
-        geom_point(size = 5, alpha = 0.8) + 
+        geom_point(size = 3, alpha = 0.8) + 
         guides(color = guide_legend(override.aes = list(alpha = 1))) +
         theme_bw() + theme(aspect.ratio = 1,
             axis.text = element_text(color = "black"),
