4ef33277423cf94caad69056f29804d30aa12e56
HelenaLC <helena@crowells.eu>
2019-08-10 09:31:35 +0200

    simplify pbDS unit-tests

--- tests/testthat/test-pbDS.R
+++ tests/testthat/test-pbDS.R
@@ -19,10 +19,10 @@ ng <- length(gids <- levels(sce$group_id))
 g3 <- sce$group_id == "g3"
 g23 <- sce$group_id %in% c("g2", "g3")
 
-# sample 'nde' genes & multiply counts by 10 for 'g3'-cells
+# sample 'nde' genes & multiply counts by 10 for 'g2'- & 'g3'-cells
 foo <- sce
 degs <- sample(rownames(foo), (nde <- 5))
-assay(foo[degs, g3]) <- assay(foo[degs, g3]) * 10
+assay(foo[degs, g23]) <- assay(foo[degs, g23]) * 10
 pb <- aggregateData(foo, assay = "counts", fun = "sum")
 
 for (method in eval(as.list(args(pbDS))$method)) {
@@ -37,12 +37,6 @@ for (method in eval(as.list(args(pbDS))$method)) {
         expect_true(all(vapply(top, setequal, y = degs, logical(1))))
     })
 }
-
-# sample 'nde' genes & multiply counts by 10 for 'g2'- & 'g3'-cells
-foo <- sce
-degs <- sample(rownames(foo), (nde <- 5))
-assay(foo[degs, g23]) <- assay(foo[degs, g23]) * 10
-pb <- aggregateData(foo, assay = "counts", fun = "sum")
 
 # specify design & contrast matrix
 ei <- metadata(sce)$experiment_info
