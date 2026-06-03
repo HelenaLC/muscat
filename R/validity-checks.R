bbe8bf9e3f497bc712954b2f07844da66324a434
plger <pl.germain@gmail.com>
2026-06-03 11:05:13 +0200

    added shrinkage option and extra free args to downstream methods (#53)

--- R/validity-checks.R
+++ R/validity-checks.R
@@ -160,6 +160,7 @@
         is.numeric(u$min_cells), length(u$min_cells) == 1,
         is.logical(u$verbose), length(u$verbose) == 1,
         is.logical(u$treat), length(u$treat) == 1)
+    stopifnot(length(u$shrinkLFC)==1)
 }
 
 .check_args_mmDS <- function(u) {
